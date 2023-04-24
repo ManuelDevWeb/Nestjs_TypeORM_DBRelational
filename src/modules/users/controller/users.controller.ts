7d689923e9b54dcfe048a457dea277ad07f88bcd
ManuelDev <manueldeveloper17@gmail.com>
2023-04-23 23:57:54 -0500

    (Feature) Add service and controller functions for users

--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -1,4 +1,125 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  Param,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  Res,
+  Body,
+} from '@nestjs/common';
+// Importando Pipe pesonalizado
+import { ParseIntPipe } from '../../../common/parse-int/parse-int.pipe';
+// Importando DTO (Data Transfer Object)
+import { CreateUserDto, UpdateUserDto } from '../../../dtos/users.dtos';
+
+// Importando response para manipular la res con express
+import { Response } from 'express';
+
+// Imporando el servicio de users
+import { UsersService } from '../service/users.service';
+
+// Los decoradores indican como se va a comportar la clase o el metodo
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    // Inyectando dependencias (Se crea automaticamente una instancia de la clase UsersService y se inyecta en el constructor)
+    private usersService: UsersService,
+  ) {}
+
+  @Get('/')
+  // Indicamos que vamos a tener un HttpCode y enviamos un parametro del obketo HttpStatus
+  @HttpCode(HttpStatus.OK)
+  // Indicamos que vamos a recibir parametros de tipo query
+  getUsers(@Query('limit') limit = 100, @Query('offset') offset = 50) {
+    try {
+      const users = this.usersService.findAll();
+
+      return {
+        body: {
+          data: users,
+          pagination: {
+            limit,
+            offset,
+          },
+        },
+      };
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Get('/:userId')
+  // Indicamos que vamos a recibir un parámetro llamado userId (Lo convertimos en numerico gracias al pipe ParseIntPipe) y accediendo al responde de tipo Response Express
+  getUser(@Res() res: Response, @Param('userId', ParseIntPipe) userId: number) {
+    try {
+      const user = this.usersService.findOne(userId);
+
+      res.status(HttpStatus.OK).send({
+        body: {
+          data: user,
+        },
+      });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  @Post('/')
+  @HttpCode(HttpStatus.OK)
+  // Indicamos que vamos a recibir data en el body
+  createUser(@Body() payload: CreateUserDto) {
+    try {
+      const newUser = this.usersService.create(payload);
+
+      return {
+        body: {
+          data: newUser,
+        },
+      };
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Put('/:userId')
+  @HttpCode(HttpStatus.OK)
+  // Indicamos que vamos a recibir data en el body y un parametro llamado userId (Lo convertimos en numerico gracias al pipe ParseIntPipe)
+  editUser(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body() payload: UpdateUserDto,
+  ) {
+    try {
+      const userEdited = this.usersService.update(userId, payload);
+
+      return {
+        body: {
+          data: userEdited,
+        },
+      };
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Delete('/:userId')
+  @HttpCode(HttpStatus.OK)
+  // Indicamos que vamos a recibir un parametro llamado userId (Lo convertimos en numero gracias al pipe ParseIntPipe)
+  deleteUser(@Param('userId', ParseIntPipe) userId: number) {
+    try {
+      const userDeleted = this.usersService.delete(userId);
+
+      return {
+        body: {
+          data: userDeleted,
+        },
+      };
+    } catch (error) {
+      return error;
+    }
+  }
+}
--- src/modules/users/service/users.service.ts
+++ src/modules/users/service/users.service.ts
@@ -1,4 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 
-@Injectable()
-export class UsersService {}
+// Importando la clase (interface) User
+import { User } from '../entity/user.entity';
+
+// Importando el DTO (Data Transfer Object)
+import { CreateUserDto, UpdateUserDto } from '../../../dtos/users.dtos';
+
+@Injectable() // Indicamos que la clase puede ser inyectada en otros lugares
+export class UsersService {
+  private counter = 1;
+
+  // Array privado de users, solo accesible desde la clase
+  private users: User[] = [
+    {
+      id: 1,
+      email: 'manuel@gmail.com',
+      password: '1234Ab*',
+      role: 'admin',
+    },
+  ];
+
+  // Metodo para obtener todos los usuarios
+  findAll() {
+    const users = this.users;
+
+    if (users.length === 0) {
+      throw new HttpException(`There aren't any users`, HttpStatus.NOT_FOUND);
+    }
+
+    return users;
+  }
+
+  // Metodo para obtener un usuario
+  findOne(id: number) {
+    const user = this.users.find((item) => item.id === id);
+
+    if (!user) {
+      throw new HttpException(
+        `User with id ${id} doesn't exist`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return user;
+  }
+
+  // Metodo para crear un usuario
+  create(payload: CreateUserDto) {
+    this.counter++;
+
+    const newUser = {
+      id: this.counter,
+      ...payload,
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+
+  // Metodo para actualizar un usuario
+  update(id: number, payload: UpdateUserDto) {
+    const user = this.findOne(id);
+
+    if (user) {
+      // Obtenemos el indice del elemento
+      const index = this.users.findIndex((item) => item.id === id);
+
+      this.users[index] = {
+        ...user,
+        ...payload,
+      };
+
+      return this.users[index];
+    }
+  }
+
+  // Metodo para eliminar un usuario
+  delete(id: number) {
+    const user = this.findOne(id);
+
+    if (user) {
+      // Obtenemos el indice del elemento
+      const index = this.users.findIndex((item) => item.id === id);
+
+      this.users.splice(index, 1);
+
+      return true;
+    }
+  }
+}
